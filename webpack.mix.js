00e74a1686db1b2d27a31c84fa7005f4c2d38857
Kay Domrose <kay.domrose@geenen-it-systeme.de>
2021-05-04 14:46:57 +0200

    Init

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,3 @@
-const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,8 +8,3 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
